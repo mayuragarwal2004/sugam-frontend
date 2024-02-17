4a32fe804313f48407fdc12c6691e6305ace645f
Mayur Agarwal <mayur.agarwal22@vit.edu>
2024-02-18 02:37:02 +0530

    authenticate login fix

--- src/components/context/auth/AuthState.js
+++ src/components/context/auth/AuthState.js
@@ -9,32 +9,33 @@ const AuthState = (props) => {
   const [currentUserRole, setcurrentUserRole] = useState();
 
   console.log({ currentUser, currentUserRole });
-  
+
   const getUserData = () => {
     fetch("/sugam/api/get")
-    .then((response) => {
-      console.log(response);
-      response.body
-        .getReader()
-        .read()
-        .then(({ value, done }) => {
-          new TextDecoder().decode(value)
-        })
-        .then((data) => {
-          console.log(data);
-          setcurrentUser(data);
-          if (data.staus) {
-            setcurrentUserRole(data.staus);
-          }
-        })
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      // Handle errors here if needed
-    });
+      .then((response) => {
+        console.log(response);
+        response.body
+          .getReader()
+          .read()
+          .then(({ value, done }) => {
+            new TextDecoder().decode(value);
+          })
+          .then((data) => {
+            console.log(data);
+            setcurrentUser(data);
+            if (data.staus) {
+              setcurrentUserRole(data.staus);
+            }
+          });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // Handle errors here if needed
+      });
   };
 
   useEffect(() => {
+    getUserData();
   }, []);
 
   const value = { currentUser, currentUserRole };
