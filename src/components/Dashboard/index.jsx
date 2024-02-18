e808063b8344ef74fdee23e7032077524850d7d0
Mayur Agarwal <mayur.agarwal22@vit.edu>
2024-02-18 10:21:52 +0530

    dashboard integration

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
   return (
-    <div style={{backgroundColor:'#92dbc5e1'}}>Dashboard</div>
-  )
-}
+    <div style={{ backgroundColor: "#92dbc5e1" }}>
+      <iframe
+        src="http://localhost:3000/d/ae50fe37-23a0-4515-a0bc-dd9750b1bdbd/sugam-final1?orgId=1&from=1707609600000&to=1708128000000&var-location=Kalas+-+Dhanori&var-location=Kharadi+-+Chandannagar&var-location=Yerwada&var-location=Baner+-+Balewadi+-+Pashan&var-location=Bavdhan+-+Kothrud+Depot&shareView=link"
+        title="dashboard"
+        width="100%"
+        height="1500px"
+        frameBorder="0"
+      ></iframe>
+    </div>
+  );
+};
 
-export default index
+export default index;
