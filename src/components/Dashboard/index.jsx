ab139f23854d41665064aa6ee1572e1f85fb84b5
Bhavesh Agone <agonebhavesh@gmail.com>
2024-03-12 23:54:06 +0530

    changed the grafana local instance

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -4,7 +4,7 @@ const index = () => {
   return (
     <div style={{ backgroundColor: "#92dbc5e1" }}>
       <iframe
-        src="http://localhost:3000/d/ae50fe37-23a0-4515-a0bc-dd9750b1bdbd/sugam-final1?orgId=1&shareView=link&from=1708220530333&to=1708242130335"
+        src="http://localhost:3000/d/ae50fe37-23a0-4515-a0bc-dd9750b1bdbd/sugam-final1?orgId=1&from=1710246114447&to=1710267714447"
         title="dashboard"
         width="100%"
         height="1500px"
@@ -13,5 +13,6 @@ const index = () => {
     </div>
   );
 };
+//local grafana instance :.server on lap server as localhost..
 
 export default index;
