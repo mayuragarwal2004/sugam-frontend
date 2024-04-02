c51d42e243274bbe994123b365718634f2219b48
Mayur Agarwal <mayur.agarwal22@vit.edu>
2024-04-02 17:29:33 +0530

    added progress bar to image upload

--- src/components/Complaints/components/FormImageInput.jsx
+++ src/components/Complaints/components/FormImageInput.jsx
@@ -1,8 +1,4 @@
-/* eslint-disable default-case */
-// import logo from './logo.svg';
-// import './ImageInput.css';
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import {
   getStorage,
   ref,
@@ -10,56 +6,41 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 
+import LinearProgress from "@mui/material/LinearProgress";
+import CircularProgress, {
+  CircularProgressProps,
+} from "@mui/material/CircularProgress";
+
 function FormImageInput(props) {
   const { userImage, handleImageChange } = props;
+  const [uploadProgress, setUploadProgress] = useState(undefined);
+
   function handleChange(e) {
     e.preventDefault();
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     const storage = getStorage();
     const storageRef = ref(
       storage,
       "ComplaintsImages/" + Date.now() + file.name
     );
-    // storageRef = ref(storageRef, file.name);
 
-    // 'file' comes from the Blob or File API
     const uploadTask = uploadBytesResumable(storageRef, file);
+
     uploadTask.on(
       "state_changed",
       (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        // Update progress state
         const progress =
           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-        }
+        if (progress <= 10) setUploadProgress(10);
+        else setUploadProgress(progress);
       },
       (error) => {
-        // A full list of error codes is available at
-        // https://firebase.google.com/docs/storage/web/handle-errors
-        switch (error.code) {
-          case "storage/unauthorized":
-            // User doesn't have permission to access the object
-            break;
-          case "storage/canceled":
-            // User canceled the upload
-            break;
-
-          // ...
-
-          case "storage/unknown":
-            // Unknown error occurred, inspect error.serverResponse
-            break;
-        }
+        console.error("Error uploading file:", error);
       },
       () => {
-        // Upload completed successfully, now we can get the download URL
+        setUploadProgress(undefined);
+        // Upload completed successfully, get download URL
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log("File available at", downloadURL);
           handleImageChange(downloadURL);
@@ -68,6 +49,8 @@ function FormImageInput(props) {
     );
   }
 
+  console.log({ uploadProgress });
+
   return (
     <>
       <label htmlFor="img">
@@ -75,16 +58,24 @@ function FormImageInput(props) {
       </label>
       <br />
       {userImage ? (
-        <img src={userImage} style={{ maxHeight: "100px" }} alt="img" />
-      ) : (
-        <></>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginBottom: "10px",
+          }}
+        >
+          <img src={userImage} style={{ maxHeight: "100px" }} alt="img" />
+        </div>
+      ) : null}
+      {/* Progress bar */}
+      {uploadProgress !== undefined && (
+        <LinearProgress variant="determinate" value={uploadProgress} />
+        // <CircularProgress variant="determinate" value={uploadProgress} />
+        // <progress value={uploadProgress} max="100" />
       )}
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleChange}
-      />
-      {JSON.stringify(userImage)}
+      <br />
+      <input type="file" accept="image/*" onChange={handleChange} />
     </>
   );
 }
